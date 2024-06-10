1a7dd79c4d5a056e74ba0f5ee02ded7e06dc79bd
kybermartin <spak.martin@gmail.com>
2024-06-10 22:28:56 +0200

    vytvoril som triedu Lopta a nastavil obmedzenia pohybu

--- pingpong.py
+++ pingpong.py
@@ -31,6 +31,34 @@ class Palka(sprite.Sprite):
     def draw(self):
         draw.rect(window, BLACK, (self.x, self.y, 20, 100), width=0)
 
+class Lopta(sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.x_direction = 1
+        self.y_direction = 1
+
+    def update(self):
+        if self.x > WIN_WIDTH - 5:
+            self.x_direction = -1
+        if self.x < 5:
+            self.x_direction = 1
+
+        if self.y > WIN_HEIGHT - 5:
+            self.y_direction = -1
+        if self.y < 5:
+            self.y_direction = 1
+
+        self.x += self.x_direction * 5
+        self.y += self.y_direction * 5
+
+    def draw(self):
+        draw.circle(window, WHITE, (self.x, self.y), 10)
+
+
+
+
 window = display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 display.set_caption("Ping Pong")
 clock = time.Clock()
@@ -38,6 +66,7 @@ clock = time.Clock()
 
 palkaA = Palka(10,10)
 palkaB = Palka(WIN_WIDTH - 30, 10)
+lopta = Lopta(100,100)
 
 run = True
 while run:
@@ -59,10 +88,12 @@ while run:
 
     palkaA.update()
     palkaB.update()
+    lopta.update()
 
     window.fill(RED)    
     palkaA.draw()
     palkaB.draw()
+    lopta.draw()
 
     display.update()
     clock.tick(FPS)
